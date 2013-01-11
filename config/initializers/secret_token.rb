5d3bd907ef39f24ddf5c15dac6e8c78d43815962
Vu Ngoc Anh <anhvn01457@gmail.com>
2013-01-11 14:30:02 +0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cauhang::Application.config.secret_token = 'd53cdaf2bf5efbe28d0592b070ca55bae02fc7536a0002ca6e6da177f3356536990e0df3decec1e43ba15b5df21a11b179ab1bee4ece9593ead595035022b3de'
